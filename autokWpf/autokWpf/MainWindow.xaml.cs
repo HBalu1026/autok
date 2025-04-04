0bb518d6debd56c7e2da7df8f0da1e7338542d51
Jesus Christ <71315012+HBalu1026@users.noreply.github.com>
2025-04-04 09:07:41 +0200

    Add files via upload

---
+++ autokWpf/autokWpf/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace autokWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Auto> autok = new List<Auto>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BetoltButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv|All files (*.*)|*.*"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                autok = Auto.BeolvasFajlbol(openFileDialog.FileName);
+                DataGrid.ItemsSource = autok;
+            }
+        }
+
+        private void KeresButton_Click(object sender, RoutedEventArgs e)
+        {
+            ListBoxMárkák.Items.Clear();
+            if (int.TryParse(TextBoxGyártásiÉv.Text, out int keresettÉv))
+            {
+                var szűrtAutók = autok.Where(a => a.GyártásiÉv == keresettÉv);
+                foreach (var auto in szűrtAutók)
+                {
+                    ListBoxMárkák.Items.Add($"{auto.Márka} {auto.Modell}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Érvényes gyártási évet adjon meg!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void KilepesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Biztosan ki szeretne lépni?", "Megerősítés", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ autokWpf/autokWpf/Models/Auto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autokWpf
+{
+    internal class Auto
+    {
+        int sorszám, gyártásiÉv, eladottDarabszám, átlagosEladásiÁr;
+        string márka, modell, szín;
+
+        public Auto(string auto)
+        {
+            string[] tomb = auto.Split(';');
+
+            this.sorszám = int.Parse(tomb[0]);
+            this.márka = tomb[1];
+            this.modell = tomb[2];
+            this.gyártásiÉv = int.Parse(tomb[3]);
+            this.szín = tomb[4];
+            this.eladottDarabszám = int.Parse(tomb[5]);
+            this.átlagosEladásiÁr = int.Parse(tomb[6]);
+        }
+
+        public int Sorszám { get => sorszám; }
+        public int GyártásiÉv { get => gyártásiÉv; }
+        public int EladottDarabszám { get => eladottDarabszám; }
+        public int ÁtlagosEladásiÁr { get => átlagosEladásiÁr; }
+        public string Márka { get => márka; }
+        public string Modell { get => modell; }
+        public string Szín { get => szín; }
+
+        public static List<Auto> BeolvasFajlbol(string fajlNev)
+        {
+            List<Auto> autok = new List<Auto>();
+            string[] sorok = File.ReadAllLines(fajlNev);
+
+            for (int i = 1; i < sorok.Length; i++)
+            {
+                autok.Add(new Auto(sorok[i]));
+            }
+            return autok;
+        }
+    }
+}
